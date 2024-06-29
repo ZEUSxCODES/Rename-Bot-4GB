c718ec0ae80ba2ffc7dc637f5213776dc9f658b6
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-06-29 18:32:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from bot import Bot
+import threading
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return '@JishuDeveloper'
+def home():
+    return "Bot is running!"
 
+def run_bot():
+    bot = Bot()
+    bot.run()
 
 if __name__ == "__main__":
-    app.run()
+    bot_thread = threading.Thread(target=run_bot)
+    bot_thread.start()
+    app.run(host='0.0.0.0', port=8080)
